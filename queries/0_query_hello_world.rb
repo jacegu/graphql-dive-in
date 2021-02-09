40315013828d8277f01cf80d6797bb5d0a4d790f
Javier Acero <j4cegu@gmail.com>
2021-02-09 22:21:28 +0100

    Reuse data and types

--- queries/0_query_hello_world.rb
+++ queries/0_query_hello_world.rb
@@ -1,30 +1,8 @@
 require 'graphql'
-
-class User
-  attr_reader :id, :name, :email
-
-  def initialize(id:, name:, email:)
-    @id = id
-    @name = name
-    @email = email
-  end
-end
-
-USERS = {
-  1 => User.new(id: 1, name: "john", email: "foo@bar.com")
-}
-
-module Types
-  class User < GraphQL::Schema::Object
-    field :id, ID, null: false
-    field :name, String, "John Doe", null: false
-    field :email, String, "John Doe", null: false
-  end
-end
-
+require_relative 'data'
+require_relative 'types'
 
 class QueryType < GraphQL::Schema::Object
-
   field :user, Types::User, null: true do
     description "Find a user by ID"
     argument :id, ID, required: true
@@ -48,4 +26,4 @@ query_string = <<~QUERY
 }
 QUERY
 
-pp Schema.execute(query_string).to_json
+puts JSON.pretty_generate(Schema.execute(query_string))
